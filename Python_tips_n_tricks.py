54c17a68f13a0f8f17d86aa26d547efb3f2b4fd0
lazy12316 <ashish12316@gmail.com>
2019-11-20 09:09:45 -0800

    20-Nov-2019

--- Python_tips_n_tricks.py
+++ Python_tips_n_tricks.py
@@ -1,7 +1,7 @@
 # Date:19-Nov-2019
 """
-Different ways to test multiple flags at once in python
-"""
+# Different ways to test multiple flags at once in python
+
 x,y,z = 0,1,0
 
 # Method-1
@@ -15,3 +15,20 @@ if 1 in (x,y,z):
 # Method-3
 if any((x,y,z)):
 	print("passed-Method3")
+
+
+"""	
+"""
+# How to sort a python dictionary by value (== get a representation sorted by value)
+"""
+
+# Method-1
+xs = {'a':4,'b':3,'c':2,'d':1}
+
+print("Method - 1")
+print(sorted(xs.items(), key = lambda x : x[1]))
+
+
+print("Method - 2")
+import operator
+print(sorted(xs.items(), key=operator.itemgetter(1)))
