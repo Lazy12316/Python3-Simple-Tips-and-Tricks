e2c9d733554d08992c785aa93fd8ffddacda768b
lazy12316 <ashish12316@gmail.com>
2019-11-29 08:09:59 -0800

    29-Nov-2019

--- Python_tips_n_tricks_1.py
+++ Python_tips_n_tricks_1.py
@@ -144,6 +144,27 @@ print(f(1, 2, 'p', 'q', 'v'))
 #      "f() takes 2 positional arguments but 5 were given"
 """
 
+# [🐍PyTricks]: Trick - 27
+
+"""
+# Multiple sets of kwargs in Python 3.5+.
+# Python 3.5+ allows passing multiple sets
+# of keyword arguments ("kwargs") to a
+# function within a single call, using
+# the "**" syntax:
+
+def process_data(a, b, c, d):
+	print(a, b, c, d)
+
+x = {'a': 1, 'b': 2}
+y = {'c': 3, 'd': 4}
+
+print(process_data(**x, **y))
+# >>> 1 2 3 4
+
+print(process_data(**x, c=23, d=42))
+# >>> 1 2 23 42
+"""
 
 
 
